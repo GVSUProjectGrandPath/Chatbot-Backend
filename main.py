1d02bcc0816c10e00b43a45a1cd8af9c99f7252a
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-04-10 10:43:25 -0400

    Cleaned files

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from finlit-bot!")
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Annotated,List
 
 
-if __name__ == "__main__":
-    main()
+class NewSessionResponse(BaseModel):
+    session_id : Annotated[str,Field(description="Session-Id")]
+
+class ChatResponse(BaseModel):
+    message: Annotated[[str], Field(description= "User message list")]
+    session_id: Annotated[str, Field(description="Reponse from LLM ")]
+
+
+app = FastAPI(title="Finlit-Backend-API")
+
+@app.get("/health")
+def health():
+    return {'status':'ok'}
+
